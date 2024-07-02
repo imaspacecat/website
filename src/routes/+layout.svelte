1ef2f4428e2d76e8b0d77e4f5593256cfae7d5f2
spacecat <library4lion@gmail.com>
2024-07-02 14:04:00 -0700

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,51 @@
+<script>
+    import Nav from "../components/Nav.svelte";
+    import Logo from "../components/Logo.svelte";
+</script>
+<Nav></Nav>
+<Logo size="5em" color="#000" />
+<p>asad</p>
+<!-- <nav>
+    <ul>
+      <li><a href="/">🏠</a></li>
+      <li><a href="/blog">Blog</a></li>
+      <li><a href="/cv">CV</a></li>
+      <li><a href="/about">About</a></li>
+    </ul>
+  </nav>
+  
+  <slot />
+  
+  <style>
+    nav {
+      display: flex;
+      justify-content: space-around;
+      padding: 1rem;
+      background-color: #f8f9fa;
+    }
+  
+    ul {
+      list-style: none;
+      display: flex;
+      gap: 1rem;
+      padding: 0;
+      margin: 0;
+    }
+  
+    li {
+      margin: 0;
+    }
+  
+    a {
+      text-decoration: none;
+      color: #0070f3;
+      font-weight: bold;
+    }
+  
+    a:hover {
+      text-decoration: underline;
+    }
+  </style> -->
+
+
+<slot></slot>
